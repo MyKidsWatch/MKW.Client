8a4cf8c118da419e76d8279d8b082bc93f2ac7e0
FelipeDominguesB <felipedom2001@hotmail.com>
2023-05-14 20:12:25 -0300

    fix removed dark theme

--- MKW/src/app/modules/auth/auth.module.ts
+++ MKW/src/app/modules/auth/auth.module.ts
@@ -4,9 +4,9 @@ import { AuthComponent } from './auth.component';
 import { IonicModule } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { routes } from './auth.routes';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 
 
