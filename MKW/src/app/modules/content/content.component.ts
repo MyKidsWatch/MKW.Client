c0c5e9a3d3c847ce629e542a2a840c46c4aa2971
lukkanog <lukanog@hotmail.com>
2023-05-28 01:58:42 -0300

    feat: create (static) movie page

--- MKW/src/app/modules/content/content.component.ts
+++ MKW/src/app/modules/content/content.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ContentComponent implements OnInit {
   constructor() {
 
-   }
+  }
 
   ngOnInit() {
     
-   }
+  }
+
+  goBack() {
+    window.history.back();
+  }
 
 }
