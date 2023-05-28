219434e952ff1960a3f23ae15fa679649b670a75
lukkanog <lukanog@hotmail.com>
2023-05-28 04:29:31 -0300

    feat: make content component dynamic

--- MKW/src/app/modules/content/content.component.ts
+++ MKW/src/app/modules/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../../core/services/movie.service';
 
 @Component({
   selector: 'app-content',
@@ -6,16 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent implements OnInit {
-  constructor() {
+  public movieObject?: any;
+  public loading: boolean = true;
 
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
 
   ngOnInit() {
-    
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.movieService
+      .getMovie(id)
+      .subscribe({
+        next: (res: any) => {
+          this.movieObject = res;
+          this.loading = false;
+        },
+        error: (err: any) => {
+          console.log(err);
+        }
+      });
   }
 
   goBack() {
     window.history.back();
   }
-
 }
