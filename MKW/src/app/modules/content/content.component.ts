23f28b078c115e54340eeff8bc9f57247ba0bfb9
lukkanog <lukanog@hotmail.com>
2023-05-28 12:43:38 -0300

    refactor: change content component to use ContentUtils function

--- MKW/src/app/modules/content/content.component.ts
+++ MKW/src/app/modules/content/content.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../../core/services/movie.service';
 
+import { ContentCard } from 'src/app/shared/models/content-card.model';
+import { ContentUtils } from 'src/app/core/Util/ContentUtils';
+
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent implements OnInit {
-  public movieObject?: any;
+  public movieObject?: ContentCard
   public loading: boolean = true;
 
   constructor(
@@ -20,10 +23,10 @@ export class ContentComponent implements OnInit {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.movieService
-      .getMovie(id)
+      .getMovieById(id)
       .subscribe({
         next: (res: any) => {
-          this.movieObject = res;
+          this.movieObject = ContentUtils.TMDBToContentCard(res);
           this.loading = false;
         },
         error: (err: any) => {
