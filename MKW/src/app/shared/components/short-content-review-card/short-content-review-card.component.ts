6985f2e501be3949461892c8db51c2240b0e93bc
FelipeDominguesB <felipedom2001@hotmail.com>
2023-11-22 23:08:15 -0300

    localized date

--- MKW/src/app/shared/components/short-content-review-card/short-content-review-card.component.ts
+++ MKW/src/app/shared/components/short-content-review-card/short-content-review-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { Router, RouterModule } from '@angular/router';
 import { ContentReviewCard } from '../../models/content-review-card.model';
 
@@ -14,12 +14,17 @@ import { ContentReviewCard } from '../../models/content-review-card.model';
 })
 export class ShortContentReviewCardComponent implements OnInit {
   @Input() contentData?: ContentReviewCard;
+  public dateFormat: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private translateService: TranslateService) {
+    let currentLanguage = this.translateService.currentLang;
+
+    this.dateFormat = currentLanguage === 'pt-BR' ? 'dd/MM/yyyy' : 'MM/dd/yyyy';
+  }
 
   redirectToContentReviewPage(contentId: number) {
     this.router.navigate([`home/content/review/${contentId}`]);
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 }
